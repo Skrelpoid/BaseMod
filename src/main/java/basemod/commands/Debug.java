15cf9afb52f27f994463150c1931c69c547ac409
Dustin <dustin.kristen2000@yahoo.de>
2018-11-09 16:53:42 +0100

    Debug command

---
+++ src/main/java/basemod/commands/Debug.java
@@ -0,0 +1,25 @@
+package basemod.commands;
+
+import com.megacrit.cardcrawl.core.Settings;
+import basemod.DevConsole;
+
+public class Debug extends AbstractIntermediateCommand {
+
+	public Debug() {
+		SimpleEndCommand debugTrue = new SimpleEndCommand((t, c) -> setDebugModeAndLog(true));
+		putSubCommand("true", debugTrue);
+		SimpleEndCommand debugFalse = new SimpleEndCommand((t, c) -> setDebugModeAndLog(false));
+		putSubCommand("false", debugFalse);
+	}
+	
+	private void setDebugModeAndLog(boolean debug) {
+		Settings.isDebug = debug;
+		DevConsole.log("Setting debug mode to: " + debug);
+	}
+	
+	@Override
+	public String defaultErrorMessage() {
+		return "could not parse previous command\n" + "options are:\n" + "* true\n" + "* false"; 
+	}
+
+}
